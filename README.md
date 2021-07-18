bc09af366f759e8e7b3da99df4c5b249bdd2350d
Impact-l <87244850+Impact-l@users.noreply.github.com>
2021-07-18 08:07:31 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# x8-Burp
-Hidden parameters discovery suite
+<h3 align="center">Hidden parameters discovery suite wrapper</h3>
+
+#
+
+<p align="center"><img src="https://user-images.githubusercontent.com/87244850/126019752-09dc8b3c-c6a4-49f3-9398-7974cbae3d03.png" width="50%"/></p>
+
+
+
+The tool helps to find hidden parameters that can be vulnerable or can reveal interesting functionality that other hunters miss. Greater accuracy is achieved thanks to the line-by-line comparison of pages, comparison of response code and reflections.
+
+## Features
+
+- Selecting multiple requests from the Proxy or Repeater tab.
+- Each selected request is executed in a separate thread.
+- Automatic Issue creation when hidden parameter is found.
+- HTTP/2 Support.
+- Requests with detected parameters are visible in the Proxy tab.
+- Issue is added with severity `Information` when WAF is detected.
+- Automatic detection of injection point. If the request body exists, then parameters in URL-Query are ignored.
+- Custom injectin point can be defined using `%s` or `&%s`
+<p align="center"><img src="https://user-images.githubusercontent.com/87244850/125835832-a24d2cec-4dc1-4ffd-afff-12f4e99c409c.png" width="79%"/></p>
+
+## Usage
+- There are four search choices available: 
+    - Small Wordlist (Recommended, `25000` words, 7 threads)
+    - Large Wordlist (`63000` words, 25 threads)
+    - x8083 - all request will be proxied via port 8083 (for example, you can configure the port in Burp)
+    - Debug Params - the minimum number of requests to detect only debug parameters and parameters based on response
+
+<p align="center">&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;<img src="https://user-images.githubusercontent.com/87244850/125835974-5679ec1a-0126-48a2-82ba-1861c94ed551.png" width="83%"/></p>
+
+
+## Test
+Feel free to check whether the tool works as expected and compare it with other tools at https://4rt.one/. There are 2 reflected parameters, 4 parameters that change code/headers/body, and one extra parameter with a not random value.
+
+## Detected parameters
+<p align="center"><img src="https://user-images.githubusercontent.com/87244850/125836031-681af553-c3ef-4314-a431-dd34e49fdb86.png" width="87%"/></p>
+
+## Acknowledgement
+Thanks to [Sh1Yo](https://github.com/Sh1Yo) for the wonderful x8 utility. He added special functions into it so that we could write this wrapper. We also spotted some bugs, specifically in HTTP/2, for Burp Suite compatibility. To examine and understand the project in detail, or if you need a command line version, click [here](https://github.com/Sh1Yo/x8).
+
+## Installation
+- Linux
+    - from releases
+        ```bash
+        Burp -> Extender -> ./x8-Burp/src/linux_x8.py
+        ```
+- Windows
+    - from releases
+        ```bash
+        Burp -> Extender -> ./x8-Burp/src/win_x8.py
+        ```
---
+++ src/win_x8.py
@@ -0,0 +1,261 @@
+__version__ = '1.01'
+__description__ = """\
+Bruteforce via x8 - Hidden parameters discovery suite
+"""
+
+# Burp imports
+from burp import IBurpExtender, ITab, IContextMenuFactory
+
+from javax import swing
+from java.awt import BorderLayout
+from java.util import ArrayList
+import time
+from burp import IScanIssue
+import os
+import sys
+import subprocess
+import shlex
+import threading
+
+try:
+    from exceptions_fix import FixBurpExceptions
+except ImportError:
+    pass
+
+class BurpExtender(IBurpExtender, ITab, IContextMenuFactory):
+    proxy = 'http://127.0.0.1:8083' # custom proxy example: http://p.webshare.io ?
+    threadCsmall = "7"
+    threadClarge = "25"
+
+    def registerExtenderCallbacks(self, callbacks):
+        #os.environ['http_proxy'] = proxy 
+        #os.environ['HTTP_PROXY'] = proxy
+        #os.environ['https_proxy'] = proxy
+        #os.environ['HTTPS_PROXY'] = proxy
+        sys.stdout = callbacks.getStdout()
+
+        self.callbacks = callbacks
+
+        self.helpers = callbacks.getHelpers()
+
+        # Set our extension name
+        self.callbacks.setExtensionName("X8")
+        self.global_issues = {}
+
+        # Create a context menu
+        callbacks.registerContextMenuFactory(self)
+ 
+
+        return
+    
+    def consolidateDuplicateIssues(self, existingIssue, newIssue):
+        if existingIssue.getUrl() == newIssue.getUrl():
+            return -1
+
+        return 0
+    # Implement ITab
+    def getTabCaption(self):
+        """Return the text to be displayed on the tab"""
+        return "X8 Params"
+    
+    def getUiComponent(self):
+        """Passes the UI to burp"""
+        return self.tab
+
+    def createMenuItems(self, invocation):
+        menuListt = ArrayList()
+        self.context = invocation
+        itemContext = invocation.getSelectedMessages()
+        if itemContext > 0:
+            menuList = ArrayList()
+            parentMenu = swing.JMenu("Send " +str(len(itemContext))+"x8 Params")
+            menuItemSmallWordList = swing.JMenuItem("Small WordList",
+                                        actionPerformed=self.handleHttpTrafficSmall)
+            menuItemLargeWordList = swing.JMenuItem("Large WordList",
+                                        actionPerformed=self.handleHttpTrafficLarge)
+            menuItemProxy = swing.JMenuItem("Small via Proxy x8083",
+                                        actionPerformed=self.handleHttpTrafficProxy)
+            menuItemDebug = swing.JMenuItem("Debug Params",
+                                        actionPerformed=self.handleHttpTrafficDebug)            
+            parentMenu.add(menuItemSmallWordList)
+            parentMenu.add(menuItemLargeWordList)
+            parentMenu.add(menuItemProxy)
+            parentMenu.add(menuItemDebug)
+            menuList.add(parentMenu)
+            return menuList
+        return None
+    
+    SELECT_MENU=0
+    threadsT = [None]*10000
+
+    def x8(self,event,requestIssue,argsf,origResponseCode):
+        print '-----------------------------------------------------------------------------\nBruteforce via x8 - Hidden parameters discovery suite - ' + str(self.helpers.analyzeRequest(requestIssue).getMethod()) + ' - ' + str(self.helpers.analyzeRequest(requestIssue).getUrl())+'\n--------------------------------------------------------------------------------'
+        #print argsf
+        popen = subprocess.Popen(argsf,shell=True,stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        popen.wait()
+        output = popen.stdout.read()
+        output = output + popen.stderr.read()
+        print output
+        if (" -> " in output or "reflects: " in output) and not ("Code 429" in output):
+         issue = CustomScanIssue(requestIssue.getHttpService(),self.helpers.analyzeRequest(requestIssue).getUrl(),[self.callbacks.applyMarkers(requestIssue, None, None)],"X8 Params",output.replace("\n", "<br>"),"Low")
+         if("(PROTOCOL_ERROR" in output):
+           issue = CustomScanIssue(requestIssue.getHttpService(),self.helpers.analyzeRequest(requestIssue).getUrl(),[self.callbacks.applyMarkers(requestIssue, None, None)],"X8 Params",output.replace("\n", "<br>").replace("[!]","<br><br>Please disable HTTP/2: Project Options -> HTTP -> Enable HTTP/2 -> \"uncheck\"<br><br>[!]"),"Low")         
+         self.callbacks.addScanIssue(issue)
+        if ("Code 429" in output) or ((origResponseCode!=403) and ("Code 403" in output)):
+         issue = CustomScanIssue(requestIssue.getHttpService(),self.helpers.analyzeRequest(requestIssue).getUrl(),[self.callbacks.applyMarkers(requestIssue, None, None)],"X8 Params - WAF Found - Block","WAF Found - Block. Too Many Requests.<br>Please Use Rotate Proxy<br><br>"+output.replace("\n", "<br>"),"Information")
+         self.callbacks.addScanIssue(issue)
+        popen.stderr.close()
+        popen.stdout.close()
+ 
+    def x8ThreadControl(self,event):
+         req=0
+         while req < len(self.context.getSelectedMessages()):
+          origResponseCode = 403
+          try:
+           origResponseCode = self.helpers.analyzeResponse(self.context.getSelectedMessages()[req].getRequest()).getStatusCode()
+          except Exception:
+           print ""
+          iRequestInfo = self.helpers.analyzeRequest(self.context.getSelectedMessages()[req].getRequest())
+          prebody=self.helpers.bytesToString(self.context.getSelectedMessages()[req].getRequest())
+          b=''
+          b = self.helpers.bytesToString(prebody[iRequestInfo.getBodyOffset():])
+          if b !='':
+           b='"'+b.replace('^','^^').replace('\\','^\\').replace('^\\\"','\\\\"').replace('"','\\"').replace('>','^>').replace('<','^<').replace('|','^|').replace('&','^&')+'"'
+          url = '"'+str(self.helpers.analyzeRequest(self.context.getSelectedMessages()[req]).getUrl()).replace('^','^^').replace('\\','^\\').replace('^\\\"','\\\\"').replace('"','\\"').replace('>','^>').replace('<','^<').replace('|','^|').replace('&','^&')+'"'
+          X = iRequestInfo.getMethod()
+          i=0
+          H = ''
+          argss=["",""]
+          if self.SELECT_MENU == 1:
+             if b !='':
+              if iRequestInfo.getContentType()!=4:
+                 argss = ["x8.exe","--disable-progress-bar","--as-body","-b",b,"-X",X,"-u",str(url),"-v","1","-c",self.threadCsmall,"-w","small-wordlist.txt","--http2","--replay-proxy","http://127.0.0.1:8080"]
+              else:
+                 argss = ["x8.exe","--disable-progress-bar","-t","json","--as-body","-b",b,"-X",X,"-u",str(url),"-v","1","-c",self.threadCsmall,"-w","small-wordlist.txt","--http2","--replay-proxy","http://127.0.0.1:8080"]
+             else:
+                 argss = ["x8.exe","--disable-progress-bar","-X",X,"-u",str(url),"-v","1","-c",self.threadCsmall,"-w","small-wordlist.txt","--http2","--replay-proxy","http://127.0.0.1:8080"]
+                 
+          if self.SELECT_MENU == 2:    
+             if b !='':
+              if iRequestInfo.getContentType()!=4:
+                 argss = ["x8.exe","--disable-progress-bar","--as-body","-b",b,"-X",X,"-u",str(url),"-v","1","-c",self.threadClarge,"-w","large-wordlist.txt","--http2","--replay-proxy","http://127.0.0.1:8080"]
+              else:
+                 argss = ["x8.exe","--disable-progress-bar","-t","json","--as-body","-b",b,"-X",X,"-u",str(url),"-v","1","-c",self.threadClarge,"-w","large-wordlist.txt","--http2","--replay-proxy","http://127.0.0.1:8080"]
+             else:
+                 argss = ["x8.exe","--disable-progress-bar","-X",X,"-u",str(url),"-v","1","-c",self.threadClarge,"-w","large-wordlist.txt","--http2","--replay-proxy","http://127.0.0.1:8080"]
+                
+          if self.SELECT_MENU == 3:    
+             if b !='':
+              if iRequestInfo.getContentType()!=4:
+                 argss = ["x8.exe","--disable-progress-bar","--as-body","-b",b,"-X",X,"-u",str(url),"-v","1","-c",self.threadCsmall,"-w","small-wordlist.txt","--http2","-x",self.proxy,"--replay-proxy","http://127.0.0.1:8080"]
+              else:
+                 argss = ["x8.exe","--disable-progress-bar","-t","json","--as-body","-b",b,"-X",X,"-u",str(url),"-v","1","-c",self.threadCsmall,"-w","small-wordlist.txt","--http2","-x",self.proxy,"--replay-proxy","http://127.0.0.1:8080"]
+             else:
+                 argss = ["x8.exe","--disable-progress-bar","-X",X,"-u",str(url),"-v","1","-c",self.threadCsmall,"-w","small-wordlist.txt","--http2","-x",self.proxy,"--replay-proxy","http://127.0.0.1:8080"]
+          if self.SELECT_MENU == 4:    
+             if b !='':
+              if iRequestInfo.getContentType()!=4:
+                 argss = ["x8.exe","--disable-progress-bar","--as-body","-b",b,"-X",X,"-u",str(url),"-v","1","-c","3","--custom-parameters","debug" , "_debug" ,"source" , "admin" , "show" , "bot" , "antibot" , "antirobot" , "staging" , "test" , "testing" , "pre" , "pre-staging" , "daily" , "env" , "uat" , "anticrawl" , "recaptcha" ,"captcha", "signing" , "signature" , "enc" , "encryption" , "automation" , "disabled" , "waf" , "disable" , "security" , "dosinglesignon" , "singlesignon" , "dosso" , "sso","--http2","--replay-proxy","http://127.0.0.1:8080"]
+              else:
+                 argss = ["x8.exe","--disable-progress-bar","-t","json","--as-body","-b",b,"-X",X,"-u",str(url),"-v","1","-c","3","--custom-parameters","debug" ,"_debug" , "source" , "admin" , "show" , "bot" , "antibot" , "antirobot" , "staging" , "test" , "testing" , "pre" , "pre-staging" , "daily" , "env" , "uat" , "anticrawl" , "captcha" , "recaptcha","signing" , "signature" , "enc" , "encryption" , "automation" , "disabled" , "waf" , "disable" , "security" , "dosinglesignon" , "singlesignon" , "dosso" , "sso","--http2","--replay-proxy","http://127.0.0.1:8080"]
+             else:
+                 argss = ["x8.exe","--disable-progress-bar","-X",X,"-u",str(url),"-v","1","-c","3","--custom-parameters","debug" , "_debug" ,"source" , "admin" , "show" , "bot" , "antibot" , "antirobot" , "staging" , "test" , "testing" , "pre" , "pre-staging" , "daily" , "env" , "uat" , "anticrawl" , "captcha" , "recaptcha","signing" , "signature" , "enc" , "encryption" , "automation" , "disabled" , "waf" , "disable" , "security" , "dosinglesignon" , "singlesignon" , "dosso" , "sso","--http2","--replay-proxy","http://127.0.0.1:8080"]
+          
+          argss.append('-H')
+          for header in iRequestInfo.getHeaders():
+              if i>0 and not header.lower().startswith("Content-Length:".lower()) and not header.lower().startswith("Host:".lower()):
+               argss.append('"'+header.replace('\\','\\\\').replace('"','\\"')+'"')
+              i=i+1
+          argsf = ' '.join(argss)
+          if '%s' in url:
+              argsf=argsf.replace(" --as-body","").replace("-t json ","")
+          self.threadsT[req] = threading.Thread(target=self.x8, args=(None,self.context.getSelectedMessages()[req],argsf,origResponseCode,))
+          self.threadsT[req].start()
+          #self.thread1.join()
+          req=req+1
+
+
+    def writeRequestToTextBox(self):
+        self.threadControl = threading.Thread(target=self.x8ThreadControl, args=(None,))
+        self.threadControl.start()
+
+
+    def handleHttpTrafficSmall(self, event):
+        self.SELECT_MENU=1
+        
+        self.writeRequestToTextBox()
+        httpTraffic = self.context.getSelectedMessages()
+        for item in httpTraffic:
+            self.httpService = item.getHttpService()
+
+    def handleHttpTrafficLarge(self, event):
+        self.SELECT_MENU=2
+        
+        self.writeRequestToTextBox()
+        httpTraffic = self.context.getSelectedMessages()
+        for item in httpTraffic:
+            self.httpService = item.getHttpService()
+
+    def handleHttpTrafficProxy(self, event):
+        self.SELECT_MENU=3
+        
+        self.writeRequestToTextBox()
+        httpTraffic = self.context.getSelectedMessages()
+        for item in httpTraffic:
+            self.httpService = item.getHttpService()
+            
+    def handleHttpTrafficDebug(self, event):
+        self.SELECT_MENU=4
+        
+        self.writeRequestToTextBox()
+        httpTraffic = self.context.getSelectedMessages()
+        for item in httpTraffic:
+            self.httpService = item.getHttpService()
+
+
+try:
+    FixBurpExceptions()
+except:
+    pass
+
+class CustomScanIssue (IScanIssue):
+    def __init__(self, httpService, url, httpMessages, name, detail, severity):
+        self._httpService = httpService
+        self._url = url
+        self._httpMessages = httpMessages
+        self._name = name
+        self._detail = detail
+        self._severity = severity
+
+    def getUrl(self):
+        return self._url
+
+    def getIssueName(self):
+        return self._name
+
+    def getIssueType(self):
+        return 0
+
+    def getSeverity(self):
+        return self._severity
+
+    def getConfidence(self):
+        return "Firm"
+
+    def getIssueBackground(self):
+        pass
+
+    def getRemediationBackground(self):
+        pass
+
+    def getIssueDetail(self):
+        return self._detail
+
+    def getRemediationDetail(self):
+        pass
+
+    def getHttpMessages(self):
+        return self._httpMessages
+
+    def getHttpService(self):
+        return self._httpService
